f3cc05cc73d9fdbfd74466b63d207f6c07ebf1a4
Mikhail Kormyshov <kormyshov@gmail.com>
2022-08-02 19:43:05 +0300

    Add debug output

--- database.py
+++ database.py
@@ -29,6 +29,12 @@ class Database(AbstractBase):
             )
 
         result = self.pool.retry_operation_sync(select)
+        print('ok')
+        print(result)
+        print(len(result))
+        print(len(result[0]))
+        print(result[0])
+        
         if len(result[0]) == 0:
             raise UserDoesntExistInDB
 
