825700f8dcb4cd04388ec5ca6ffe975830f035d6
Mikhail Kormyshov <kormyshov@gmail.com>
2022-10-26 21:15:16 +0200

    Add typing to tuple of actions

--- controller/controller.py
+++ controller/controller.py
@@ -1,7 +1,9 @@
+from typing import Tuple
 from logging_decorator import logger
 from abstract_base import AbstractBase
 from abstract_viewer import AbstractViewer
 from user import User
+from controller.abstract_action import AbstractAction
 from controller.look_sets_action import LookSetsAction
 from controller.request_to_look_set_info_action import RequestToLookSetInfoAction
 from controller.request_to_add_set_action import RequestToAddSetAction
@@ -17,7 +19,7 @@ from controller.delete_set_action import DeleteSetAction
 
 class Controller:
 
-    actions = (
+    actions: Tuple[AbstractAction] = (
         LookSetsAction(),
         RequestToLookSetInfoAction(),
         RequestToAddSetAction(),
