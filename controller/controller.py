1ba7dd5b6ff74985e6e3d96b03f0f4622a5ed9a6
Mikhail Kormyshov <kormyshov@gmail.com>
2022-10-26 19:43:00 +0200

    Fix path to module in controller

--- controller/controller.py
+++ controller/controller.py
@@ -2,17 +2,17 @@ from logging_decorator import logger
 from abstract_base import AbstractBase
 from abstract_viewer import AbstractViewer
 from user import User
-from look_sets_action import LookSetsAction
-from request_to_look_set_info_action import RequestToLookSetInfoAction
-from request_to_add_set_action import RequestToAddSetAction
-from request_to_delete_set_action import RequestToDeleteSetAction
-from request_to_add_exist_set_action import RequestToAddExistSetAction
-from request_to_add_new_set_action import RequestToAddNewSetAction
-from go_to_main_menu_action import GoToMainMenuAction
-from add_exist_set_action import AddExistSetAction
-from add_new_set_action import AddNewSetAction
-from look_set_info_action import LookSetInfoAction
-from delete_set_action import DeleteSetAction
+from controller.look_sets_action import LookSetsAction
+from controller.request_to_look_set_info_action import RequestToLookSetInfoAction
+from controller.request_to_add_set_action import RequestToAddSetAction
+from controller.request_to_delete_set_action import RequestToDeleteSetAction
+from controller.request_to_add_exist_set_action import RequestToAddExistSetAction
+from controller.request_to_add_new_set_action import RequestToAddNewSetAction
+from controller.go_to_main_menu_action import GoToMainMenuAction
+from controller.add_exist_set_action import AddExistSetAction
+from controller.add_new_set_action import AddNewSetAction
+from controller.look_set_info_action import LookSetInfoAction
+from controller.delete_set_action import DeleteSetAction
 
 
 class Controller:
