fe7b5cb3a7369336e3419434ef06932fa65c20d8
Mikhail Kormyshov <kormyshov@gmail.com>
2022-11-16 18:57:55 +0300

    Add replacing term in samples

--- controller/request_term_by_sample_action.py
+++ controller/request_term_by_sample_action.py
@@ -1,3 +1,4 @@
+import re
 from logging_decorator import logger
 from controller.abstract_action import AbstractAction
 from abstract_viewer import AbstractViewer
@@ -5,6 +6,14 @@ from user import User
 from constants import CANCEL, ENTER_TERM_WITH_SAMPLE, RIGHT
 import keyboards
 from controller.request_term_by_definition_action import RequestTermByDefinitionAction
+
+
+def replace_term_to_stars(sample: str) -> str:
+    return re.sub(
+        r'\[.+?\]',
+        lambda m: re.sub(r'\S', '*', m.group().strip('[]')),
+        sample,
+    )
 
 
 class RequestTermBySampleAction(AbstractAction):
@@ -23,4 +32,4 @@ class RequestTermBySampleAction(AbstractAction):
         else:
             sample = user.request_term_by_sample()
             viewer.view(user.id, ENTER_TERM_WITH_SAMPLE)
-            viewer.view(user.id, sample, keyboards.get_cancel())
+            viewer.view(user.id, replace_term_to_stars(sample), keyboards.get_cancel())
