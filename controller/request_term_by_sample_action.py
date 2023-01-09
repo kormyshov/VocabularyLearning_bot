e6fa3f6ca07b1605fe97a65cc96242de8c920ba4
Mikhail Kormyshov <kormyshov@gmail.com>
2023-01-09 22:50:53 +0300

    Add second attempt if term without sample fo using

--- controller/request_term_by_sample_action.py
+++ controller/request_term_by_sample_action.py
@@ -3,7 +3,7 @@ from logging_decorator import logger
 from controller.abstract_action import AbstractAction
 from abstract_viewer import AbstractViewer
 from user import User
-from constants import CANCEL, IT_IS_USED_IN, RIGHT
+from constants import CANCEL, IT_IS_USED_IN, RIGHT, WITHOUT_SAMPLE, SECOND_ATTEMPT
 import keyboards
 from controller.request_term_by_definition_action import RequestTermByDefinitionAction
 
@@ -34,5 +34,8 @@ class RequestTermBySampleAction(AbstractAction):
             RequestTermByDefinitionAction().do(viewer, user, '')
         else:
             sample = user.request_term_by_sample()
-            viewer.view(user.id, IT_IS_USED_IN)
-            viewer.view(user.id, replace_term_to_stars(sample), keyboards.get_cancel())
+            if sample != WITHOUT_SAMPLE:
+                viewer.view(user.id, IT_IS_USED_IN)
+                viewer.view(user.id, replace_term_to_stars(sample), keyboards.get_cancel())
+            else:
+                viewer.view(user.id, SECOND_ATTEMPT, keyboards.get_cancel())
