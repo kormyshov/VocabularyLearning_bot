f666522185fda8d7d3e209c4429d0e632537dcdc
Mikhail Kormyshov <kormyshov@gmail.com>
2023-04-12 21:00:09 +0300

    Add debug output

--- index.py
+++ index.py
@@ -4,15 +4,22 @@ from main import bot
 
 
 def handler(event, _):
+    print('ok 1')
     logging.getLogger().setLevel(logging.INFO)
+    print('ok 2')
     root_handler = logging.getLogger().handlers[0]
+    print('ok 3')
     root_handler.setFormatter(logging.Formatter(
         '[%(levelname)s]\t%(request_id)s\t%(name)s\t%(message)s\n'
     ))
+    print('ok 4')
     logging.info('Program started')
+    print('ok 5')
 
     message = telebot.types.Update.de_json(event['body'])
+    print('ok 6')
     bot.process_new_updates([message])
+    print('ok 7')
     return {
         'statusCode': 200,
         'body': '!',
